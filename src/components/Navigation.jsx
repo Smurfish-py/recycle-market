202848480832f6a97f2ef0da0355ec32e6ff021b
smurfish-py <m.rifki0729@gmail.com>
2025-10-19 12:25:49 +0700

    adding navigation component

---
+++ src/components/Navigation.jsx
@@ -0,0 +1,52 @@
+import { useState, useEffect, useRef } from "react"
+import { ChevronDownIcon, ChevronUpIcon } from "@heroicons/react/24/outline"
+
+function Navigation({ className, listStyle }) {
+    const [ isOpen, setIsOpen ] = useState(false);
+    const navigationList = ["Elektronik", "Non-elektronik"]
+    
+    const dropdownRef = useRef(null);
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setIsOpen(false);
+            }
+        }
+        document.addEventListener('mousedown', handleClickOutside);
+
+        return () => {
+            document.removeEventListener('mousedown', handleClickOutside)
+        };
+    }, [])
+
+    let content = (
+        <div className={`${listStyle} ${!isOpen ? "hidden" : "visible"} flex flex-col`}>
+            {navigationList.map((navList, index) => (
+                <a href="" key={index} className="p-2 active:bg-stone-100 hover:bg-stone-100 cursor-pointer">{navList}</a>
+                
+            ))}
+        </div>
+    );
+
+    return (
+        <nav ref={dropdownRef}>
+            <div className={`${className} flex flex-row select-none`}>
+                <a href="">Produk</a>
+                <a href="">Toko</a>
+                <a href="">Mitra</a>
+                <div className="relative">
+                    <div onClick={() => setIsOpen(!isOpen)} className="flex flex-row gap-2 items-center cursor-pointer">
+                        <p>Kategori</p>
+                        <i>
+                            {!isOpen ? (<ChevronDownIcon className="size-3 stroke-3"></ChevronDownIcon>) : (<ChevronUpIcon className="size-3 stroke-3"></ChevronUpIcon>)}
+                        </i>
+                    </div>
+                    { content }
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default Navigation
