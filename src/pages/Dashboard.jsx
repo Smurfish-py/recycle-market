f4f92f182f050763241f254fa55a67a727280577
smurfish-py <m.rifki0729@gmail.com>
2025-10-20 10:59:55 +0700

    minor update for dashboard page structure

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,10 +1,10 @@
-import Navigation from "../components/Navigation";
+import Navigation from "@/components/Navigation";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 
 function Dashboard() {
     return (
-        <main className="flex items-center justify-center">
-            <div className="w-full flex gap-3 flex-col min-[320px]:px-2 min-[480px]:max-sm:px-12 sm:px-0">
+        <main className="flex flex-col items-center gap-4">
+            <section className="w-full flex gap-3 flex-col min-[320px]:px-2 min-[480px]:max-sm:px-12 sm:px-0 lg:hidden">
                 <Navigation className="not-sr-only min-[1280px]:sr-only flex justify-between sm:justify-center sm:gap-16" listStyle="dropdown absolute w-40 right-0 sm:-left-2 translate-y-2"/>
                 <form className="not-sr-only sm:sr-only">
                     <div className="relative">
@@ -14,7 +14,10 @@ function Dashboard() {
                         <input type="text" className="pl-8 pr-4 input-text-solid font-poppins w-full" placeholder="Cari yang kamu butuhkan"></input>
                     </div>
                 </form>
-            </div>
+            </section>
+            <article className="border w-full">
+                <p>Don't forget to SMILE! :D</p>
+            </article>
         </main>
     )
 }
