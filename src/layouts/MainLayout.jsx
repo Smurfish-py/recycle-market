94df2a9486ad7890d200c115fd173f2cdfb442db
smurfish-py <m.rifki0729@gmail.com>
2025-10-20 10:59:21 +0700

    change size to minimal screen size and adding footer component

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,13 +1,15 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 
 export default function MainLayout() {
     return (
-        <>
+        <div className="flex flex-col min-h-screen">
             <Header />
-            <main className="px-4 py-4">
+            <main className="px-4 py-2 grow">
                 <Outlet />
             </main>
-        </>
+            <Footer />
+        </div>
     )
 }
