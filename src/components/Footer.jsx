5d78f6c11b51eb2f81dcc270e96b19009181bb6d
smurfish-py <m.rifki0729@gmail.com>
2025-10-21 11:32:53 +0700

    minor changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -30,40 +30,46 @@ function Footer() {
             </section>
 
             {/* Navigasi */}
-            <section className='flex-1 sr-only md:not-sr-only md:flex md:flex-col md:gap-6'>
-                <h1 className='sr-only font-inter text-xl font-semibold md:not-sr-only'>Navigasi</h1>
-                <div className='flex flex-col gap-2 font-poppins'>
-                    <a href=''>Beranda</a>
-                    <a href=''>Produk</a>
-                    <a href=''>Kontak Kami</a>
-                    <a href=''>FAQ</a>
+            <section className='flex-1 sr-only md:not-sr-only md:flex md:justify-center'>
+                <div className='flex flex-col md:gap-6'>
+                    <h1 className='sr-only font-inter text-xl font-semibold md:not-sr-only'>Navigasi</h1>
+                    <div className='flex flex-col gap-2 font-poppins'>
+                        <a href=''>Pengembang</a>
+                        <a href=''>Beranda</a>
+                        <a href=''>Produk</a>
+                        <a href=''>FAQ</a>
+                    </div>
                 </div>
+                
             </section>
 
             {/* Kontak */}
-            <section className="flex flex-row flex-1 text-xs items-center justify-between md:flex-col md:justify-normal md:items-start md:gap-6 md:text-sm">
-                <h1 className='sr-only font-inter text-xl font-semibold md:not-sr-only'>Kontak</h1>
-                <div className='flex flex-col gap-2 font-poppins'>
-                    <div className='flex flex-row gap-2 items-center'>
-                        <EnvelopeIcon className='size-4 stroke-2' />
-                        <p>support@recyclemarket.com</p>
+            <section className="flex flex-1 justify-center">
+                <div className='flex flex-rowtext-xs items-center justify-between md:flex-col md:justify-normal md:items-start md:gap-6 md:text-sm'>
+                    <h1 className='sr-only font-inter text-xl font-semibold md:not-sr-only'>Kontak</h1>
+                    <div className='flex flex-col gap-2 font-poppins'>
+                        <div className='flex flex-row gap-2 items-center'>
+                            <EnvelopeIcon className='size-4 stroke-2' />
+                            <p>support@recyclemarket.com</p>
+                        </div>
+                        <div className='flex flex-row gap-2 items-center'>
+                            <PhoneIcon className='size-4 stroke-2' />
+                            <p>+62 812-3456-7890</p> 
+                        </div>
                     </div>
-                    <div className='flex flex-row gap-2 items-center'>
-                        <PhoneIcon className='size-4 stroke-2' />
-                        <p>+62 812-3456-7890</p> 
+                    <div className='flex flex-row gap-2'>
+                        <a href='https://github.com/Smurfish-py/recycle-market'>
+                            <FontAwesomeIcon icon="fa-brands fa-github" size='2x' />
+                        </a>
+                        <a href='https://www.notion.so/RECYCLE-MARKET-Perencanaan-269ea26622cb80dd9b78dfef638487a7?source=copy_link'>
+                            <FontAwesomeIcon icon="fa-brands fa-notion" size='2x' />
+                        </a>
+                        <a href='https://www.figma.com/design/SOAcmeZ1SqIPhaIqzm6Nm3/Recycle-Market---Kelompok-13?node-id=0-1&t=d9WD5m9IUeZOqOj4-1'>
+                            <FontAwesomeIcon icon="fa-brands fa-figma" size='2x' />
+                        </a>
                     </div>
                 </div>
-                <div className='flex flex-row gap-2'>
-                    <a href='https://github.com/Smurfish-py/recycle-market'>
-                        <FontAwesomeIcon icon="fa-brands fa-github" size='2x' />
-                    </a>
-                    <a href='https://www.notion.so/RECYCLE-MARKET-Perencanaan-269ea26622cb80dd9b78dfef638487a7?source=copy_link'>
-                        <FontAwesomeIcon icon="fa-brands fa-notion" size='2x' />
-                    </a>
-                    <a href='https://www.figma.com/design/SOAcmeZ1SqIPhaIqzm6Nm3/Recycle-Market---Kelompok-13?node-id=0-1&t=d9WD5m9IUeZOqOj4-1'>
-                        <FontAwesomeIcon icon="fa-brands fa-figma" size='2x' />
-                    </a>
-                </div>
+                
             </section>
         </footer>
     )   
