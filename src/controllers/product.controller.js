20c8d37dab4555159ad8efb9477bfc5b842c37c0
smurfish-py <m.rifki0729@gmail.com>
2025-10-26 20:13:43 +0700

    adding fetch product list function

---
+++ src/controllers/product.controller.js
@@ -0,0 +1,14 @@
+import axios from "axios"
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const findAllProducts = async () => {
+    try {
+        const products = await axios.get(`${API_URL}/api/produk`);
+        return products;
+    } catch (error) {   
+        console.error({ error: error });
+    }
+}
+
+export default findAllProducts
