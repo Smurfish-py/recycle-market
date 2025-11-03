9b7e891d397e0a641a526105f3cbe8daf38720ec
smurfish-py <m.rifki0729@gmail.com>
2025-11-03 21:00:21 +0700

    adding product page to product route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
+
+// Layout
 import MainLayout from "./layouts/MainLayout"
+
+// Pages
 import Dashboard from "./pages/Dashboard"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
+import Product from "./pages/Product"
 
 function App() {
   return (
@@ -13,6 +18,7 @@ function App() {
         </Route>
         <Route path="/login" element={ <Login /> }></Route>
         <Route path="/register" element={ <Register /> }></Route>
+        <Route path="/product/:id" element={ <Product /> }></Route>
       </Routes>
     </BrowserRouter>
   )
