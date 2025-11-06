ac68e4654fe6c5641776b936f70a813a14d9066d
smurfish-py <m.rifki0729@gmail.com>
2025-11-06 13:59:41 +0700

    minor change

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -3,6 +3,7 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 
 export default function MainLayout() {
+    
     return (
         <div className="relative flex flex-col min-h-screen">
             <Header isOnProductPage={false} />
