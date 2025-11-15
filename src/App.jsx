230bf5d1e02eea118802c44e38ef4beca891f530
smurfish-py <m.rifki0729@gmail.com>
2025-11-15 11:10:42 +0700

    New: add routes for ShopPage, PartnershipPage, and Profile in App component

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ import Dashboard from "./pages/Dashboard"
 import Login from "./pages/Login"
 import Register from "./pages/Register"
 import Product from "./pages/Product"
-import ProductByCathegory from "./pages/ProductByCategory"
+import ProductByCathegory from "./pages/ProductByCategory";
+import ShopPage from "./pages/ShopPage"
+import Profile from "./pages/Profile"
+import PartnershipPage from "./pages/PartnershipPage"
 
 function App() {
   return (
@@ -17,10 +20,14 @@ function App() {
         <Route element={ <MainLayout /> }>
           <Route path="/" element={ <Dashboard /> }></Route>
           <Route path="/category/:category" element={ <ProductByCathegory /> }></Route>
+          <Route path="/shop/:shop" element={ <ShopPage /> }></Route>
+          <Route path="/dashboard/shop"></Route>
+          <Route path="/partnership" element={ <PartnershipPage /> }></Route>
         </Route>
         <Route path="/login" element={ <Login /> }></Route>
         <Route path="/register" element={ <Register /> }></Route>
         <Route path="/product/:id" element={ <Product /> }></Route>
+        <Route path="/profile" element={ <Profile /> }></Route>
       </Routes>
     </BrowserRouter>
   )
