572ff764149a9aec058b795d7e278b31ff1ae1fc
smurfish-py <m.rifki0729@gmail.com>
2025-11-15 11:13:56 +0700

    fix: remove unused import from Dashboard component

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, use } from "react";
+import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import Navigation from "@/components/Navigation";
 import { findAllProducts } from "../controllers/product.controller";
