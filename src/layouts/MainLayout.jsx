6769497e496621e7d624d38a2c92bffde0e409fb
smurfish-py <m.rifki0729@gmail.com>
2025-11-23 21:57:28 +0700

    feat: pass queryResults state to Header and Outlet for improved data handling

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,14 +1,16 @@
 import { Outlet } from "react-router-dom";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { useState } from "react";
 
 export default function MainLayout() {
-    
+    const [ queryResults, setQueryResults ] = useState([]);
+
     return (
         <div className="relative flex flex-col min-h-screen">
-            <Header customHeader={false} />
+            <Header customHeader={false} sendToParent={setQueryResults} />
             <main className="px-4 py-2 grow">
-                <Outlet />
+                <Outlet context={{ queryResults }}/>
             </main>
             <Footer />
         </div>
