54aa901895241319eba37acd37683068a0116aa4
smurfish-py <m.rifki0729@gmail.com>
2025-11-23 21:57:10 +0700

    feat: add search and related product functionality in product controller

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -4,7 +4,7 @@ const API_URL = import.meta.env.VITE_API_URL;
 
 const findAllProducts = async () => {
     try {
-        const products = await axios.get(`${API_URL}/api/produk`);
+        const products = await axios.get(`${API_URL}/api/produk/`);
         return products;
     } catch (error) {   
         console.error({ error: error });
@@ -29,4 +29,56 @@ const findProductByCategory = async (category) => {
     }
 }
 
-export { findAllProducts, findProductId, findProductByCategory }
+const searchProduct = async (productName) => {
+    try {
+        const product = await axios.get(`${API_URL}/api/produk/search/${productName}`);
+        return product.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const findRelatedProduct = async (id) => {
+    try {
+        const product = await axios.get(`${API_URL}/api/produk/toko/${id}`);
+        return product.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const addProduct = async ( formData ) => {
+    try {
+        const res = await axios.post(
+            `${API_URL}/api/produk/add`,
+            formData,
+            {
+                headers: {
+                    "Content-Type": "multipart/form-data"
+                }
+            }
+        );
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const deleteProductById = async (id) => {
+    try {
+       const res = await axios.delete(`${API_URL}/api/produk/delete/${id}`, {timeout: 60000});
+        return res; 
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { 
+    findAllProducts, 
+    findProductId, 
+    findProductByCategory, 
+    searchProduct,
+    findRelatedProduct,
+    addProduct,
+    deleteProductById
+}
