1f616dfc01bd97156d95d3a9512821a499d471b8
smurfish-py <m.rifki0729@gmail.com>
2025-11-23 21:58:06 +0700

    feat: add Profile page with user data fetching and profile update functionality

---
+++ src/pages/Profile.jsx
@@ -0,0 +1,81 @@
+import Footer from "../components/Footer";
+import Header from "../components/Header";
+
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
+
+import isTokenExpired from "../service/isTokenExpired";
+import { userData } from "../controllers/user.controller";
+
+import { UserCircleIcon, UserIcon } from "@heroicons/react/24/outline";
+
+export default function Profile() {
+    const [ user, setUser ] = useState([]);
+
+    const navigate = useNavigate();
+    const token = localStorage.getItem('token');
+    const decode = jwtDecode(token);
+    
+
+    useEffect(() => {
+        token == null || isTokenExpired(token) ? navigate('/login') : "";
+
+        const fetchData = async (id) => {
+            try {
+                const user = await userData( id );
+                setUser(user?.data);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+
+        fetchData(decode?.id);
+    }, []);
+
+    return (
+        <section className="relative min-h-screen flex flex-col">
+            <Header customHeader={true} title={"Profil Pengguna"} />
+            <main className="grow py-16 lg:py-24 px-4 flex flex-row justify-center">
+                <div className="border border-stone-300 rounded-lg w-80 py-6 md:w-120">
+                    <div className="flex flex-col items-center w-full gap-4">
+                        <h1 className="text-2xl font-inter font-semibold">BIODATA PENGGUNA</h1>
+                        <UserCircleIcon className="size-24"/>
+                        <div>
+                            <h2 className="text-base font-poppins">{user.fullname}</h2>
+                            <h4 className="text-sm text-center">Privilege : {user?.privilege?.[0]?.privilege}</h4>
+                        </div>
+                    </div>
+                    <div className="px-4 mt-8">
+                        <h2 className="font-semibold">Ubah Profil</h2>
+                        <hr className="px-4 my-2" />
+                        <form className="flex flex-col justify-center">
+                            <div>
+                                <label htmlFor="fullname">Nama Lengkap</label>
+                                <input name="fullname" className="input-text w-full mb-3" defaultValue={user.fullname} placeholder="Contoh: Budi Santoso"/>
+                                <label htmlFor="username">Nama Pengguna</label>
+                                <input name="username" className="input-text w-full mb-3" defaultValue={user.username} placeholder="Contoh: Budi01"/>
+                                <label htmlFor="notelp">Nomor Telepon</label>
+                                <input name="notelp" className="input-text w-full mb-3" defaultValue={user.noHp} placeholder="Contoh: 08123456789"/>
+                                <label htmlFor="alamat">Alamat Rumah</label>
+                                <input name="alamat" className="input-text w-full mb-3" defaultValue={user.alamat} placeholder="Contoh: Jl. melati no.3 rt02 rw01"/>
+                            </div>
+                            <div className="flex flex-col gap-2">
+                                <button type="submit" className="btn-solid cursor-pointer">Perbarui Profil</button>
+                                <button className="btn-solid bg-red-400 border-none cursor-pointer" onClick={(e) => {
+                                    e.preventDefault();
+                                    localStorage.clear()
+                                    navigate('/');
+                                }}>
+                                    Logout
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    
+                </div>
+            </main>
+            <Footer />
+        </section>
+    )
+}
