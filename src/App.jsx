02d35558c9a00e0ddeab2d7c8d8b5cafd2e374b8
Smurfish-py <m.rifki0729@gmail.com>
2026-01-17 21:13:42 +0700

    refactor: clean up imports and fix typo in ProductByCategory component

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,14 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 // Layout
-import MainLayout from "./layouts/MainLayout"
+import MainLayout from "./layouts/MainLayout";
 
 // Pages
-import Dashboard from "./pages/Dashboard"
-import Login from "./pages/Login"
-import Register from "./pages/Register"
-import Product from "./pages/Product"
-import ProductByCathegory from "./pages/ProductByCategory";
-import ShopPage from "./pages/ShopPage"
-import Profile from "./pages/Profile"
-import PartnershipPage from "./pages/PartnershipPage"
-import SellForm from "./pages/SellForm"
-import Admin from "./pages/Admin"
+import { Dashboard, Login, Register, Profile, PartnershipPage, Product, ProductByCategory } from "@/pages/default/index.js"
+import ShopPage from "./pages/ShopPage";
+import SellForm from "./pages/SellForm";
+import { Admin, UserListAdmin } from "@/pages/admin/index";
+import ManagementLayout from "./layouts/ManagementLayout";
 
 function App() {
   return (
@@ -21,12 +16,20 @@ function App() {
       <Routes>
         <Route element={ <MainLayout /> }>
           <Route path="/" element={ <Dashboard /> }></Route>
-          <Route path="/category/:category" element={ <ProductByCathegory /> }></Route>
+          <Route path="/category/:category" element={ <ProductByCategory /> }></Route>
           <Route path="/shop/:shop" element={ <ShopPage /> }></Route>
           <Route path="/dashboard/shop" element={<ShopPage />}></Route>
           <Route path="/partnership" element={ <PartnershipPage /> }></Route>
           <Route path="/sell" element={<SellForm />}></Route>
-          <Route path="/dashboard/admin" element={<Admin />}></Route> 
+        </Route>
+        <Route element={ <ManagementLayout /> }>
+        {/* Admin */}
+          <Route path="/dashboard/admin" element={<Admin />}></Route>
+          <Route path="/dashboard/admin/pengguna" element={ <UserListAdmin /> }></Route>
+          <Route path="/dashboard/admin/produk"></Route>
+          <Route path="/dashboard/admin/pengaduan"></Route>
+          <Route path="/dashboard/admin/pesanan"></Route>
+        {/* Partner */}
         </Route>
         <Route path="/login" element={ <Login /> }></Route>
         <Route path="/register" element={ <Register /> }></Route>
