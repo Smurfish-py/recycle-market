4025cfd91f9406f349dc304ab083058a88f7acd6
Smurfish-py <m.rifki0729@gmail.com>
2026-01-17 21:17:41 +0700

    feat: add UserListPage component for displaying user information and management actions

---
+++ src/pages/admin/UserList.jsx
@@ -0,0 +1,74 @@
+import { useState, useEffect } from "react";
+import { findAllUsers } from "@/controllers/user.controller";
+import { PencilSquareIcon } from "@heroicons/react/24/outline";
+
+export default function UserListPage() {
+    const [ usersList, setUsersList ] = useState([]); 
+    const styles = {
+        admin: "bg-sky-100 px-4 py-1 rounded-sm font-semibold text-sm text-sky-400",
+        partner: "bg-green-accent text-green-main-2 px-4 py-1 rounded-sm font-semibold text-sm",
+        default: "bg-zinc-200 text-zinc-400 px-4 py-1 rounded-sm font-semibold text-sm"
+    }
+
+    useEffect(() => {
+        const fetchAllAccount = async () => {
+            const users = await findAllUsers();
+            setUsersList(users.data);
+            console.log(users.data)
+        }
+
+        fetchAllAccount();
+    }, [findAllUsers]);
+
+    return (
+        <section className="w-full grid grid-cols-1">
+            <div className="card border-1 border-zinc-300 flex flex-col py-4 px-6">
+                <h3 className="text-xl">Daftar Pengguna</h3>
+                <p className="text-sm text-zinc-400"><strong>Catatan:</strong> untuk user mobile, geser ke kanan untuk melihat kolom aksi</p>
+                <br />
+                <div className="w-full overflow-y-hidden flex justify-center flex-col gap-3 rounded-md">
+                    <table className="
+                        border border-zinc-300 border-collapse
+                        [&_tr]:h-10 [&_tr]:border [&_tr]:border-zinc-300
+                        [&_th]:px-2 [&_th]:border [&_th]:border-zinc-300
+                        [&_td]:px-2 [&_td]:w-fit [&_td]:border [&_td]:border-zinc-300 
+                    ">
+                        <thead className="bg-zinc-300 text-left font-inter">
+                            <tr>
+                                <th>No</th>
+                                <th>Id</th>
+                                <th>Nama Lengkap</th>
+                                <th>Username</th>
+                                <th>Email</th>
+                                <th>Privilege</th>
+                                <th>Aksi</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            { usersList.map((user, index) => (
+                                <tr key={user?.id} className="even:bg-zinc-50">
+                                    <td>{index + 1}</td>
+                                    <td>{user?.id}</td>
+                                    <td>{user?.fullname}</td>
+                                    <td>{user?.username}</td>
+                                    <td>{user?.email}</td>
+                                    <td>
+                                        <span className={styles[user?.privilege?.[0].privilege.toLowerCase()]}>{user?.privilege?.[0].privilege}</span>
+                                    </td>
+                                    <td>
+                                        <div className="w-full flex justify-center gap-2">
+                                            <button className="cursor-pointer p-1 rounded-md hover:bg-zinc-200 active::bg-zinc-200">
+                                                <PencilSquareIcon className="size-6 stroke-1" />
+                                            </button>
+                                        </div>
+                                        
+                                    </td>
+                                </tr>
+                            )) }
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </section>
+    )
+}
