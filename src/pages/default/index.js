089aed3f417ee5522a822bf5c1de4f3cdd5ce4b5
Smurfish-py <m.rifki0729@gmail.com>
2026-01-18 13:17:16 +0700

    feat: update export for Profile to EditProfile in index.js

--- src/pages/default/index.js
+++ src/pages/default/index.js
@@ -1,7 +1,7 @@
 export { default as Dashboard } from './Dashboard';
 export { default as Login } from './Login';
 export { default as Register } from './Register';
-export { default as Profile } from './Profile';
+export { default as EditProfile } from './EditProfile';
 export { default as PartnershipPage } from './PartnershipPage';
 export { default as Product } from './Product';
 export { default as ProductByCategory } from './ProductByCategory';
