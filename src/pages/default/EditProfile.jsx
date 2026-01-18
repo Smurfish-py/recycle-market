c22addbf4a484eebb26a18a7db3e08ba00c18990
Smurfish-py <m.rifki0729@gmail.com>
2026-01-18 13:17:08 +0700

    feat: add EditProfile component for user profile management and updates

--- src/pages/default/EditProfile.jsx
+++ src/pages/default/EditProfile.jsx
@@ -1,22 +1,52 @@
-import Footer from "../../components/Footer";
-import Header from "../../components/Header";
+import Footer from "@/components/Footer";
+import Header from "@/components/Header";
 
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { jwtDecode } from "jwt-decode";
 
-import isTokenExpired from "../../service/isTokenExpired";
-import { userData } from "../../controllers/user.controller";
+import isTokenExpired from "@/service/isTokenExpired";
+import { userData, updateUser } from "@/controllers/user.controller";
 
-import { UserCircleIcon, UserIcon } from "@heroicons/react/24/outline";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
 
-export default function Profile() {
+const API_URL = import.meta.env.VITE_API_URL;
+
+export default function EditProfile() {
     const [ user, setUser ] = useState([]);
+    const [ file, setFile ] = useState(null);
 
     const navigate = useNavigate();
     const token = localStorage.getItem('token');
     const decode = jwtDecode(token);
     
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const {fullname, username, noHp, alamat} = e.target;
+
+        const formData = new FormData();
+
+        formData.append('fullname', fullname.value);
+        formData.append('username', username.value);
+        formData.append('noHp', noHp.value);
+        formData.append('alamat', alamat.value);
+        formData.append('profilePfp', file);
+        try {
+            const updateProfile = async (data) => {
+                const res = await updateUser(decode.id, data);
+                return res;
+            }
+
+            updateProfile(formData);
+        } catch (error) {
+            console.log(error.data);
+        }
+    }
 
     useEffect(() => {
         token == null || isTokenExpired(token) ? navigate('/login') : "";
@@ -40,7 +70,12 @@ export default function Profile() {
                 <div className="border border-stone-300 rounded-lg w-80 py-6 md:w-120">
                     <div className="flex flex-col items-center w-full gap-4">
                         <h1 className="text-2xl font-inter font-semibold">BIODATA PENGGUNA</h1>
-                        <UserCircleIcon className="size-24"/>
+                        {user.profilePfp == undefined || user.profilePfp == null ? <UserCircleIcon className="size-24"/> : (
+                            <>
+                                <img src={`${API_URL}/api/images/users/${user?.profilePfp}`} />
+                            </>
+                        )}
+                        
                         <div>
                             <h2 className="text-base font-poppins">{user.fullname}</h2>
                             <h4 className="text-sm text-center">Privilege : {user?.privilege?.[0]?.privilege}</h4>
@@ -49,19 +84,21 @@ export default function Profile() {
                     <div className="px-4 mt-8">
                         <h2 className="font-semibold">Ubah Profil</h2>
                         <hr className="px-4 my-2" />
-                        <form className="flex flex-col justify-center">
+                        <form className="flex flex-col justify-center" onSubmit={handleSubmit}>
                             <div>
                                 <label htmlFor="fullname">Nama Lengkap</label>
                                 <input name="fullname" className="input-text w-full mb-3" defaultValue={user.fullname} placeholder="Contoh: Budi Santoso"/>
                                 <label htmlFor="username">Nama Pengguna</label>
                                 <input name="username" className="input-text w-full mb-3" defaultValue={user.username} placeholder="Contoh: Budi01"/>
-                                <label htmlFor="notelp">Nomor Telepon</label>
-                                <input name="notelp" className="input-text w-full mb-3" defaultValue={user.noHp} placeholder="Contoh: 08123456789"/>
+                                <label htmlFor="noHp">Nomor Telepon</label>
+                                <input name="noHp" className="input-text w-full mb-3" defaultValue={user.noHp} placeholder="Contoh: 08123456789"/>
                                 <label htmlFor="alamat">Alamat Rumah</label>
                                 <input name="alamat" className="input-text w-full mb-3" defaultValue={user.alamat} placeholder="Contoh: Jl. melati no.3 rt02 rw01"/>
+                                <label htmlFor="profilePfp">Foto Profil</label> <br />
+                                <input name="profilePfp" type="file" accept="image/*" onChange={handleFileChange} />
                             </div>
                             <div className="flex flex-col gap-2">
-                                <button type="submit" className="btn-solid cursor-pointer">Perbarui Profil</button>
+                                <button type="submit" className="btn-solid cursor-pointer active:bg-green-800">Perbarui Profil</button>
                                 <button className="btn-solid bg-red-400 border-none cursor-pointer" onClick={(e) => {
                                     e.preventDefault();
                                     localStorage.clear()
@@ -71,8 +108,7 @@ export default function Profile() {
                                 </button>
                             </div>
                         </form>
-                    </div>
-                    
+                    </div> 
                 </div>
             </main>
             <Footer />
