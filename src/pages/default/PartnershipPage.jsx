0970e05c0bad355e149bfd198a685d888a38e569
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-01-30 22:11:14 +0700

    Add navigation for partnership registration and policy access in PartnershipPage

--- src/pages/default/PartnershipPage.jsx
+++ src/pages/default/PartnershipPage.jsx
@@ -1,14 +1,13 @@
+import { useNavigate } from 'react-router-dom';
+import { jwtDecode } from 'jwt-decode';
+
 import { 
     ScaleIcon, 
     BuildingStorefrontIcon, 
     CheckBadgeIcon, 
     HeartIcon, 
-    UserIcon, 
-    IdentificationIcon,
-    DocumentTextIcon
 } from '@heroicons/react/24/solid';
 
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -17,9 +16,10 @@ import { fab } from '@fortawesome/free-brands-svg-icons';
 
 library.add(fas, far, fab);
 
-import partnerShipImg from '@/assets/images/partnership.jpg'
+import partnerShipImg from '@/assets/images/partnership.jpg';
 
 export default function PartnershipPage() {
+    const navigate = useNavigate();
     return (
         <section className='mt-16 flex justify-center'>
             <div className='w-full sm:w-2/3'>
@@ -35,7 +35,7 @@ export default function PartnershipPage() {
                     <div className='px-2'>
                         <h1 className='text-4xl md:text-4xl font-semibold mb-4'>GREENSHIFT PARTNERSHIP PROGRAM</h1>
                         <p className='mb-4'><span>Greenshift Partnership</span> merupakan program kesepakatan dan kerja sama antara pihak pengaju dengan kami dengan tujuan untuk menjual barang dan berafiliasi dengan kami. Pengguna yang ingin memulai penjualan barang di website ini wajib mendaftar program ini</p>
-                        <a className='btn-solid w-fit cursor-pointer'>DAFTAR PARTNERSHIP</a>
+                        <a className='btn-solid w-fit cursor-pointer' onClick={() => navigate(`/partnership/form/`)}>DAFTAR PARTNERSHIP</a>
                     </div>
                     <div className='px-2'>
                         <h2 className='text-2xl font-semibold mb-3'>Manfaat Menjadi Greenshift</h2>
@@ -70,50 +70,18 @@ export default function PartnershipPage() {
                             </div>
                         </div>
                     </div>
-                    <hr className='text-stone-300'/>
-                    <div className='px-2'>
-                        <h2 className='text-2xl font-semibold mb-3'>Syarat Menjadi Greenshift</h2>
-                        <div className='grid grid-cols-1 md:grid-cols-2 md:gap-2'>
-                            <div className='px-2 py-2 flex flex-row items-center gap-4 bg-green-accent'>
-                                <div className='flex-1/6 flex items-center justify-center'>
-                                    <UserIcon className='size-8'/>
-                                </div>
-                                
-                                <h3 className='flex-5/6'>Pengguna terdaftar setidaknya sudah berumur 13 tahun</h3>
-                            </div>
-                            <div className='px-2 py-2 flex flex-row items-center gap-4 bg-emerald-50'>
-                                <div className='flex-1/6 flex items-center justify-center'>
-                                    <IdentificationIcon className='size-8'/>
-                                </div>
-                                
-                                <h3 className='flex-5/6'>Foto KTP sebagai jaminan jika terjadi sesuatu yang tak diinginkan. Pengguna dengan umur dibawah 17 tahun dapat menggunakan KTP wali untuk melengkapi syarat pendaftaran</h3>
-                            </div>
-                            <div className='px-2 py-2 flex flex-row items-center gap-4 bg-green-accent'>
-                                <div className='flex-1/6 flex items-center justify-center'>
-                                    <FontAwesomeIcon icon="fa-brands fa-whatsapp" size="2x"/>
-                                </div>
-                                
-                                <h3 className='flex-5/6'>Memiliki nomor WhatsApp Business</h3>
-                            </div>
-                            <div className='px-2 py-2 flex flex-row items-center gap-4 bg-emerald-50'>
-                                <div className='flex-1/6 flex items-center justify-center'>
-                                    <DocumentTextIcon className='size-8'/>
-                                </div>
-                                
-                                <h3 className='flex-5/6'>Mengikuti semua kebijakan yang kami berikan kepada anda</h3>
-                            </div>
-                        </div>
-                    </div>
                     <hr className='text-stone-300' />
                     <div className='px-2'>
                         <h2 className='text-2xl font-semibold mb-3'>Sudah Siap Menjadi Greenshift?</h2>
                         <div className='grid grid-cols-1 md:grid-cols-2'>
                             <div className='py-2 mb-4'>
                                 <h2 className='text-xl font-semibold'>Perlu Diperhatikan</h2>
-                                <p className='mb-4'>Dengan mendaftar, anda setuju untuk memberikan informasi pribadi seperti KTP dan Nomor Telepon WhatsApp anda. Kami tidak akan menjual informasi tersebut dan hanya menggunakannya untuk keperluan pendaftaran partner saja.</p>
+                                <p className='mb-4'>Dengan mendaftar, anda setuju untuk memberikan informasi pribadi anda. Verifikasi identitas mitra digunakan untuk mencegah penipuan dan sebagai bukti hukum dalam kerja sama.</p>
                                 <div className='grid grid-cols-1 gap-2'>
-                                    <a className='btn w-fit cursor-pointer'>Baca Peraturan dan Kebijakan</a>
-                                    <a className='btn-solid w-fit cursor-pointer'>DAFTAR PARTNERSHIP</a>
+                                    <a className='btn w-fit cursor-pointer' onClick={() => {
+                                        navigate('/partnership/policy')
+                                    }}>Baca Peraturan dan Kebijakan</a>
+                                    <a className='btn-solid w-fit cursor-pointer' onClick={() => navigate(`/partnership/form/${decode?.id}`)}>DAFTAR PARTNERSHIP</a>
                                 </div>
                             </div>
                         </div>
