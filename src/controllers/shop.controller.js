e8de39605b7864256b52ced6c4c72430533e7000
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-01-30 21:44:54 +0700

    Adding new controller (shop.controller.js)

---
+++ src/controllers/shop.controller.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+const registerShop = async (data) => {
+    try {
+        const res = await axios.post(`${API_URL}/api/toko/create`, data);
+        return res
+    } catch (error) {
+        throw error
+    }
+}
+
+const findShopData = async (id) => {
+    try {
+        const res = await axios.get(`${API_URL}/api/toko/data/${id}`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+const findShopDataByUser = async (id) => {
+    try {
+        const res = await axios.get(`${API_URL}/api/toko/data/user/${id}`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
+}
+
+export { registerShop, findShopData, findShopDataByUser }
