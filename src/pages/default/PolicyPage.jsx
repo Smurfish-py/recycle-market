0c1efe26403005badcdef1e5df871b43c7ad7f95
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-01-30 22:11:33 +0700

    Adding policy page for registering partner

---
+++ src/pages/default/PolicyPage.jsx
@@ -0,0 +1,85 @@
+export default function PolicyPage() {
+    return (
+        <div className="min-h-screen min-w-screen flex justify-center items-start py-4">
+            <div className="max-w-150">
+                <h1 className="text-2xl font-semibold text-center">KEBIJAKAN GREENSHIFT PARTNERSHIP</h1>
+                <p className="text-center">Kebijakan per 22 Januari 2026</p>
+                <br />
+                <p>Greenshift Partnership merupakan program kesepakatan dan kerja sama antara pihak pengaju dengan kami dengan tujuan untuk menjual barang dan berafiliasi dengan kami. Pengguna yang ingin memulai penjualan barang di website ini wajib mendaftar program ini. Berikut adalah syarat dan ketentuan yang perlu anda pahami sebelum mendaftar menjadi partner: </p>
+                <br />
+                <h2 className="text-xl font-semibold">1. Persyaratan Umum</h2>
+                <ol className="list-decimal pl-8">
+                    <li>Partner wajib memberikan data yang benar, akurat, dan dapat dipertanggungjawabkan.</li>
+                    <li>Partner bersedia mematuhi seluruh Syarat & Ketentuan serta Kebijakan Privasi website.</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">2. Data yang Dikumpulkan</h2>
+                <p>Untuk keperluan partnership dan verifikasi, partner wajib mengisi:</p>
+                <ol className="list-disc pl-8">
+                    <li>Nama Lengkap sesuai Identitas (Pastikan anda menggunakan nama lengkap pada akun anda)</li>
+                    <li>Nama toko</li>
+                    <li>Deskripsi singkat mengenai toko anda</li>
+                    <li>Nomor whatsapp business aktif</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">3. Proses Verifikasi Partner</h2>
+                <p>Partner akan melalui proses verifikasi berikut:</p>
+                <ol className="list-decimal pl-8 mb-2">
+                    <li>Verifikasi nama lengkap</li>
+                    <li>Verifikasi nama dan deskripsi toko</li>
+                    <li>Verifikasi nomor telepon (konfirmasi Admin)</li>
+                    <li>Waktu verifikasi maksimal 1-5 hari kerja.</li>
+                    <li>Keputusan platform bersifat final</li>
+                </ol>
+                <p>Website berhak:</p>
+                <ol className="list-disc pl-8">
+                    <li>Menolak partnership jika data tidak valid</li>
+                    <li>Meminta klarifikasi tambahan bila diperlukan</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">4. Aturan Penjualan Barang Bekas</h2>
+                <ol className="list-decimal pl-8">
+                    <li>Barang yang dijual <strong>bukan hasil pencurian, penipuan, atau tindakan ilegal.</strong></li>
+                    <li>Kondisi barang harus dijelaskan dengan jujur dan transparan.</li>
+                    <li>Foto barang harus asli, jelas, dan sesuai barang.</li>
+                    <li>Partner bertanggung jawab penuh atas: Keaslian barang, kondisi barang, kesesuaian deskripsi</li>
+                    <li>Dilarang menjual: Barang Ilegal, Barang tanpa hak kepemilikan, Barang yang melanggar hukum</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">5. Pencegahan Penipuan</h2>
+                <ol className="list-decimal pl-8">
+                    <li>Website berhak melakukan: Pembatasan akun, Penangguhan transaksi, dan Penonaktifan partnership</li>
+                    <li>Partner bertanggung jawab penuh atas: Keaslian barang, kondisi barang, kesesuaian deskripsi</li>
+                    <li>Dilarang menjual: Barang Ilegal, Barang tanpa hak kepemilikan, Barang yang melanggar hukum</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">6. Sistem Pembayaran & Komisi</h2>
+                <ol className="list-decimal pl-8">
+                    <li>Platform mengambil komisi sebesar 10% dari harga jual</li>
+                    <li>Partner tidak akan dikenakan biaya komisi apabila bertransaksi secara offline (COD/Cash on Delivery)</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">7. Perlindungan Data</h2>
+                <ol className="list-decimal pl-8">
+                    <li>Data partner dijaga sesuai kebijakan privasi.</li>
+                    <li>Data tidak akan dibagikan ke pihak ketiga tanpa izin, kecuali diwajibkan hukum.</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">8. Perubahan Kebijakan</h2>
+                <ol className="list-decimal pl-8">
+                    <li>Platform berhak mengubah kebijakan sewaktu-waktu.</li>
+                    <li>Perubahan akan diberitahukan melalui website atau email.</li>
+                </ol>
+                <br />
+                <h2 className="text-xl font-semibold">9. Persetujuan</h2>
+                <p>Dengan mendaftar sebagai partner, Anda dianggap:</p>
+                <ol className="list-decimal pl-8">
+                    <li>Telah membaca</li>
+                    <li>Memahami</li>
+                    <li>Menyetujui seluruh kebijakan ini</li>
+                </ol>
+            </div>
+            
+        </div>
+    )
+}
