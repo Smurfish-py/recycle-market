6e9646c673a7aacf6ae697a5e5dc1f976a51d728
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-01-30 22:12:37 +0700

    Adding index.js for better export and import

---
+++ src/pages/partner/index.js
@@ -0,0 +1,4 @@
+export { default as SellForm } from '@/pages/partner/SellForm';
+export { default as ShopPage } from '@/pages/partner/ShopPage';
+export { default as ShopEdit } from '@/pages/partner/ShopEdit';
+export { default as PartnershipForm } from '@/pages/partner/PartnershipForm';
