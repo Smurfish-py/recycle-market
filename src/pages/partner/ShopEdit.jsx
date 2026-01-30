03b51b052367bc91bc6837ee51a1af3bac1c01e3
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-01-30 22:15:41 +0700

    Adding shop edit for editing shop info / image

---
+++ src/pages/partner/ShopEdit.jsx
@@ -0,0 +1,9 @@
+export default function shopEdit() {
+    return (
+        <div className="mt-16 flex items-center justify-center w-full h-full">
+            <form className="border px-4 py-2">
+                test
+            </form>
+        </div>
+    )
+}
