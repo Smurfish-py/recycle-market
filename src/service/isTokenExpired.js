23e358212537ee1ab45daef5778a068e832461b0
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-02-16 05:02:24 +0700

    Refactor logic for decoded

--- src/service/isTokenExpired.js
+++ src/service/isTokenExpired.js
@@ -2,10 +2,18 @@ import { jwtDecode } from "jwt-decode"
 
 export default function isTokenExpired(token) {
     try {
-        const decoded = jwtDecode(token);
+        const decoded = token ? jwtDecode(token) : null;
         const currentTime = Date.now() / 1000;
         
-        return decoded.exp < currentTime;
+        if (decoded) {
+            if (decoded < currentTime) {
+                return true;
+            } else {
+                return false
+            }
+        } else {
+            return false;
+        }
     } catch (error) {
         throw error;
     }
