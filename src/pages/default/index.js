981cd9b5ed9d1519811ed34e57514f0739534075
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-03-08 12:00:00 +0700

    Adding export for transaction page

--- src/pages/default/index.js
+++ src/pages/default/index.js
@@ -8,3 +8,4 @@ export { default as Product } from './Product';
 export { default as ProductByCategory } from './ProductByCategory';
 export { default as PolicyPage } from './PolicyPage';
 export { default as BookmarkPage } from './BookmarkPage';
+export { default as BuyProductPage } from './BuyProductPage';
