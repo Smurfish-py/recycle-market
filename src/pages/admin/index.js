6c21ec80cfe74286ee689df5448c7e7e089b10b0
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-03-15 10:22:42 +0700

    add: adding export for request admin

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,3 +1,4 @@
 export { default as Admin } from './Admin';
 export { default as UserListAdmin } from './UserList';
 export { default as AdminProduct } from './AdminProducts';
+export { default as AdminRequests } from './AdminRequests';
