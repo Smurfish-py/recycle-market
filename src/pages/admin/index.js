7fe4ab374c589aaf427f7ddb397f4205d263b064
Muhamad Rifqi Kurniawan <m.rifki0729@gmail.com>
2026-03-17 10:58:57 +0700

    feat: adding exports for new pages

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -2,3 +2,6 @@ export { default as Admin } from './Admin';
 export { default as UserListAdmin } from './UserList';
 export { default as AdminProduct } from './AdminProducts';
 export { default as AdminRequests } from './AdminRequests';
+export { default as AdminOrders } from './AdminOrders';
+export { default as AdminShops } from './AdminShops';
+export { default as AdminShopDetails } from './AdminShopDetails';
